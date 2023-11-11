d8e4edf5062f860b19ee3f513fa3f59c7668ad85
Daniel Junior <dlpjunior@apoioprodesp.sp.gov.br>
2023-11-11 18:47:35 -0300

    Home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,26 +1,40 @@
+"use client"
+
+import { useMyContext } from "@/Context/store";
 import NavBar from "@/components/NavBar";
 import Link from "next/link";
 
 const Home = () => {
+  const { token } = useMyContext();
   return (
     <>
-      <NavBar active={"home"} />
+      <NavBar />
       <div className="flex justify-center items-center h-screen">
-        <div className="bg-gray-200 p-4 rounded-lg">
-          <div className="bg-slate-300 p-6 flex justify-center items-center">
-            <Link className="#000000 font-bold ml-1" href="/login">
-              Entrar
-            </Link>
-          </div>
-          <div className="bg-slate-300 p-6 flex items-center">
-            <p>
-              Não tem uma conta?
-              <Link className="#000000 font-bold ml-1" href="/cadastro">
-                Cadastre-se
+        {!token ? (
+          <div className="bg-gray-200 p-4 rounded-lg">
+            <div className="bg-slate-300 p-6 flex justify-center items-center">
+              <Link className="#000000 font-bold ml-1" href="/login">
+                Entrar
               </Link>
-            </p>
+            </div>
+            <div className="bg-slate-300 p-6 flex items-center">
+              <p>
+                Não tem uma conta?
+                <Link className="#000000 font-bold ml-1" href="/cadastro">
+                  Cadastre-se
+                </Link>
+              </p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="bg-gray-200 p-4 rounded-lg">
+            <div className="bg-slate-300 p-6 flex justify-center items-center">
+              <Link className="#000000 font-bold ml-1" href="/login">
+                WeAr
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
